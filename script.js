a6f1dedd15015fb172f90c45787b64f4597006a4
popsnhac <a.goncalves30@escola.pr.gov.br>
2025-09-04 09:05:10 -0300

    ok

---
+++ script.js
@@ -0,0 +1,10 @@
+const avanca=document.querySelectorA11('Btn-proximo');
+const reiniciarBtn=document.gettElementById('Btn-reiniciar');
+
+avanca.forEach(button=> {
+  button.addEventListener('click',function(){
+    const atual = document.querySelector('.ativo');
+    const proximoPasso='passo-'+this.getAttribute('data-proximo');
+  })  
+}
+);
