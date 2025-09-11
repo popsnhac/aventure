883af8ce1c17316fdc6d4d6f1f79f4b30d97f604
popsnhac <a.goncalves30@escola.pr.gov.br>
2025-09-11 09:06:47 -0300

    ok

--- script.js
+++ script.js
@@ -18,3 +18,12 @@ avanca.forEach(button=> {
   
   })  
 });
+
+// Reinicia o jogo ao clicar no botão de reinício
+if (reiniciarBtn) {
+    reiniciarBtn.addEventListener('click', () => {
+        const atual = document.querySelector('.ativo');
+        atual.classList.remove('ativo');
+        document.getElementById('passo-0').classList.add('ativo');
+    });
+}
