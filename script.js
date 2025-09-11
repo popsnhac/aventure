70d1af06e5a10bc62b1d303a68cd6b77d6a16371
popsnhac <a.goncalves30@escola.pr.gov.br>
2025-09-11 08:44:24 -0300

    ok

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ avanca.forEach(button=> {
   button.addEventListener('click',function(){
     const atual = document.querySelector('.ativo');
     const proximoPasso='passo-'+this.getAttribute('data-proximo');
+
+    atual.classList.remove('ativo');
+    const proximoElemento= document.getElementoById(proximoPasso);
+
+  if(proximoElemento){
+    proximoElemento.classList.add('ativo');
+  }else{
+    console.error(`Elemento com Id "${proximoPasso}" não encontrado´)
+  }
+  
+  
   })  
-}
-);
+});
